56b4006f420dac999b44cbf978bda7d29c758ffb
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-10-10 20:56:25 -0400

    Update portfolio

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Software Engineer", "Deep Learning (AI) Practioner", "Compiler Design Practioner"],
+			strings: ["Grad Student", "Deep Learning (ML) Practioner", "Compiler Builder"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
